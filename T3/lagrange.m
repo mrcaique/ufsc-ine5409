364344f086c0e7d3aae2348be0fbea06adde17cb
CaiqueMarques <caiquermarques95@gmail.com>
2016-06-17 01:50:56 -0300

    lagrange: Update documentation.

--- T3/lagrange.m
+++ T3/lagrange.m
@@ -1,3 +1,18 @@
+%{
+   Find the coeficients from Lagrange basis polyninials.
+
+   Args:
+        xpoints : points in x between the interval [a, b]
+        n : number of points
+        x : x points in tabulated table
+        y : y points in tabulated table
+
+   Returns:
+       l : y aprox. for each x
+
+   See:
+        https://en.wikipedia.org/wiki/Lagrange_polynomial    
+%}
 function l = lagrange(xpoints, n, x, y)
     l = 0;
     for i = 1 : n + 1
@@ -10,4 +25,3 @@ function l = lagrange(xpoints, n, x, y)
         l = l + y(i) * prdct;
     end
 end
-
