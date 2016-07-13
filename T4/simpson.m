127ac832b2010a1893a009af1e7d9046388931b3
Caique Marques <mqcaique@gmail.com>
2016-07-13 01:33:58 +0000

    simpson: Update documentation.

--- T4/simpson.m
+++ T4/simpson.m
@@ -1,6 +1,31 @@
+%{
+    Simpson's method.
+    The Simpson's method is a technique for approximating the definite integral.
+
+    Given a function f(x) in a interval [a, b], the definite integral I
+    from this function defines the area A of the curve. This curve can be
+    approximate by several trapezium, where each trapezium is defined by two
+    points. Each trapezium are grouped two by two (with three points), defining
+    an area A of the trapezium, because of this that Simpson's method need
+    a even number of points.
+    
+    Args:
+        n : number of points
+        a : interval a
+        b : interval b
+        func : function f(x)
+    
+    Returns:
+        sn: Area of the function f(x) approximate by the Simpson's method
+    
+    See:
+        https://en.wikipedia.org/wiki/Simpson%27s_rule
+%}
 function sn = simpson(n, a, b, func)
     h = (b - a) / n;
     x = a : h : b;
+    % y = f(x) = -x ^ 2
+    % f(x) is the integrand function from the erf function
     y = func(-x .^ 2);
 
     tsum = 0;
